57352242577d079ac6e9d491d6d71518ddec42c4
Lantsev Evgeniy <lantsev_es@mail.ru>
2020-04-07 14:30:52 +0300

    end commit lesson 2

---
+++ src/ru/geekbrains/java_two/lesson_2/WrongSizeArrayExeption.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_two.lesson_2;
+
+public class WrongSizeArrayExeption extends Exception {
+    public WrongSizeArrayExeption(String message) {
+        super(message);
+    }
+}
