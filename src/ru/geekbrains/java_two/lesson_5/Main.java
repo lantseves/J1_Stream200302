cc63d07986d7b55ebde69b7d14b88b1713b451f0
Lantsev Evgeniy <lantsev_es@mail.ru>
2020-04-18 18:01:40 +0300

    fix bug method updateArray()

--- src/ru/geekbrains/java_two/lesson_5/Main.java
+++ src/ru/geekbrains/java_two/lesson_5/Main.java
@@ -5,34 +5,34 @@ import sun.net.idn.Punycode;
 import java.util.Arrays;
 
 public class Main {
-    static final int size = 10000000;
+    static final int size = 6;
     static final int h = size / 2 ;
 
     public static void main(String[] args) {
-        oneThread();
-        twoThread();
+        System.out.print(equalsArr(oneThread(), twoThread()));
     }
 
-    public static void oneThread() {
+    public static float[] oneThread() {
         float[] arr = new float[size];
         Arrays.fill(arr, 1);
 
         long a = System.currentTimeMillis();
-        updateArray(arr);
+        updateArray(arr , 0);
         long b = System.currentTimeMillis();
 
         System.out.printf("oneThread() - %.2f\n", (b - a) * 0.001f) ;
+        return arr ;
     }
 
-    public static void twoThread() {
+    public static float[] twoThread() {
         float[] arr = new float[size];
         Arrays.fill(arr, 1);
 
         long a = System.currentTimeMillis();
         float[] arr1 = Arrays.copyOfRange(arr , 0 , h) ;
         float[] arr2 = Arrays.copyOfRange(arr, h  , arr.length) ;
-        MyThread t1 = new MyThread(arr1) ;
-        MyThread t2 = new MyThread(arr2) ;
+        MyThread t1 = new MyThread(arr1 , 0) ;
+        MyThread t2 = new MyThread(arr2, h) ;
         try {
             t1.join();
             t2.join();
@@ -43,11 +43,20 @@ public class Main {
         System.arraycopy(arr2 , 0 , arr, h , h);
         long b = System.currentTimeMillis();
         System.out.printf("twoThread() - %.2f\n", (b - a) * 0.001f) ;
+        return arr ;
     }
 
-    public static void updateArray(float[] arr) {
+    public static void updateArray(float[] arr , int srcPos) {
         for(int i = 0 ; i < arr.length ; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5f) * Math.cos(0.2f + i / 5f) * Math.cos(0.4f + i / 2f));
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + (i + srcPos) / 5f) * Math.cos(0.2f + (i + srcPos)  / 5f) * Math.cos(0.4f + (i + srcPos)  / 2f));
         }
+    }
+
+    private static boolean equalsArr(float[] arr1 , float[] arr2) {
+        for (int i = 0 ; i < size ; i++) {
+            if (arr1[i] != arr2[i])
+                return false;
+        }
+        return true;
     }
 }
--- src/ru/geekbrains/java_two/lesson_5/MyThread.java
+++ src/ru/geekbrains/java_two/lesson_5/MyThread.java
@@ -2,14 +2,16 @@ package ru.geekbrains.java_two.lesson_5;
 
 public class MyThread extends Thread {
     private float[] arr ;
+    private int srcPos ;
 
-    public MyThread(float[] arr) {
+    public MyThread(float[] arr , int srcPos) {
         this.arr = arr ;
+        this.srcPos = srcPos ;
         start();
     }
 
     @Override
     public void run() {
-        Main.updateArray(arr);
+        Main.updateArray(arr, srcPos);
     }
 }
